5403437ffa0c118c457536c0f68eb251bf0b5c17
JohnsDadsSon <johnjebarrett@gmail.com>
2022-04-20 09:52:00 +0100

    icons re-added

--- src/features/menuitems.js
+++ src/features/menuitems.js
@@ -11,53 +11,53 @@ export const menuitems = [
   {
     title: "System Status",
     description: "This is the description for system status",
-    // icon: (
-    //   <SettingsSuggestTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />
-    // ),
+    icon: (
+      <SettingsSuggestTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />
+    ),
     catagory: "Operations",
   },
   {
     title: "Workflows",
     description: "This is the description for workflows",
-    // icon: <AccountTreeTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
+    icon: <AccountTreeTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
     catagory: "Operations",
   },
   {
     title: "Scheduler",
     description: "This is the description for scheduler",
-    // icon: <CalendarMonthTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
+    icon: <CalendarMonthTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
     catagory: "Operations",
   },
   {
     title: "Proxy Management",
     description: "This is the description for proxy Management",
-    // icon: <DnsTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
+    icon: <DnsTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
     catagory: "Operations",
   },
   {
     title: "Reporting",
     description: "This is the description for reporting",
-    // icon: <AssessmentTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
+    icon: <AssessmentTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
     catagory: "Operations",
   },
   {
     title: "Data Management",
     description: "This is the description for data management",
-    // icon: (
-    //   // <DataThresholdingTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />
-    // ),
+    icon: (
+      <DataThresholdingTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />
+    ),
     catagory: "Operations",
   },
   {
     title: "Data Quality Engine",
     description: "This is the description for data quality engine",
-    // icon: <MilitaryTechTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
+    icon: <MilitaryTechTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
     catagory: "Operations",
   },
   {
     title: "AI Lab",
     description: "This is the description for AI lab",
-    // icon: <SmartToyTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
+    icon: <SmartToyTwoToneIcon sx={{ color: "#352d56", fontSize: 60 }} />,
     catagory: "Operations",
   },
 ];
